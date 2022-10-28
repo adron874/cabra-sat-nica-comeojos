971d775c669ce5a25ada05683a39ca9d252fbf61
adron874 <116371862+adron874@users.noreply.github.com>
2022-10-28 18:03:26 +0200

    intento de bases

--- cabra satánica/My project/Assets/Scripts/cama.cs
+++ cabra satánica/My project/Assets/Scripts/cama.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class cama : MonoBehaviour
 {
-    public Vector2 pos_a =  10, 0 
-    public Vector2 pos_b =  0, 0 
-    public Vector2 pos_c =  -10, 0 ;
+    
+    public Vector2 a = new Vector2(10, 0);
+    public Vector2 b = new Vector2(0, 0);
+    public Vector2 c = new Vector2(-10, 0);
     // Start is called before the first frame update
     void Start()
     {
-        
+       
+
     }
+    public void mover() { }
 
     // Update is called once per frame
     void Update()
